61f7484bf7067262b3432a0a5bc585d8c5cc16dd
vishal-android-freak <yzvishal.vd@gmail.com>
2013-12-20 20:23:43 +0530

    Carbon related changes
    Change-Id: Ifd0e8d545c73f0c3d98e58487e97a0c55508f264

--- full_haida.mk
+++ full_haida.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from haida device
 $(call inherit-product, device/semc/haida/haida.mk)
@@ -28,3 +28,7 @@ PRODUCT_DEVICE := haida
 PRODUCT_BRAND := SEMC
 PRODUCT_MANUFACTURER := SEMC
 PRODUCT_MODEL := Xperia Neo V
+PRODUCT_PACKAGES += Apollo
+
+$(call inherit-product, device/semc/haida/haida.mk)
+$(call inherit-product-if-exists, vendor/semc/haida/haida-vendor.mk)
